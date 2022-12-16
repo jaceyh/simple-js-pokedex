eb48393924fdf7bc1a3c9617feec412580f9cc77
jaceyh <jacey.hallock@gmail.com>
2022-12-16 14:26:52 -0500

    populate var pokemonList with pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
-alert('Hello world');
+let pokemonList = [ amhpharos, hooh, mewtwo];
 
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+let mewtwo = {
+    name: "Mewtwo",
+    height : 6.58,
+    type: "psychic",
+}
+
+let hooh = {
+    name: "Ho-oh",
+    height: 12.5,
+    type: ["fire","flying"],
+}
+
+let ampharos = {
+    name: "Ampharos",
+    height: 4.58,
+    type: "electric",
+}
