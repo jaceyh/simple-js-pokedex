7b98d30b3ec2c17639d45109ec081904d71ab006
jaceyh <jacey.hallock@gmail.com>
2022-12-20 11:38:39 -0500

    add for loop to display pokemon name, ht, biggest

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,22 @@ let mewtwo = {
 }
 
 let pokemonList = [ampharos, hooh, mewtwo]
-    for (let i = 0; i <= pokemonList.length; i++){
-        document.write()
+
+/*print pokemon names + heights
+
+for (let i=0; i < pokemonList.length; i++){
+    document.write(pokemonList[i].name + pokemonList[i].height + "<br>")
+}
+
++/
+
+// for loop for length of array */
+for (let i=0; i < pokemonList.length; i++){
+// set condition for specific key-value of objects in array +/
+// <br> tag to display each object on a new line */
+    if(pokemonList[i].height <= 10){
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br>");
+    } else {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Woah, that's HUGE!" + "<br>")
     }
+}
