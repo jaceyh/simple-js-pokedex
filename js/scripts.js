b19b279838fde9dd5242fe38024284300ca239d8
jaceyh <jacey.hallock@gmail.com>
2022-12-20 10:54:47 -0500

    Reverted each pokemon to its own variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,22 @@
-let pokemonList = [ 
-    {
-        name: "Ampharos",
-        height: 4.58,
-        type: "electric",
-    },
+let ampharos = {
+    name: "Ampharos",
+    height: 4.58,
+    type: "electric",
+}
 
-    {    
-        name: "Ho-oh",
-        height: 12.5,
-        type: ["fire","flying"],
-    },
+let hooh = {
+    name: "Ho-oh",
+    height: 12.5,
+    type: ["fire","flying"],
+}
 
-    {
-        name: "Mewtwo",
-        height : 6.58,
-        type: "psychic",
+let mewtwo = {
+    name: "Mewtwo",
+    height : 6.58,
+    type: "psychic",
+}
+
+let pokemonList = [ampharos, hooh, mewtwo]
+    for (let i = 0; i <= pokemonList.length; i++){
+        document.write()
     }
-];
