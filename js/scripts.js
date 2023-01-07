7df18501e3ef67ae19ad6e8f1e324a8b98a598d3
jaceyh <jacey.hallock@gmail.com>
2023-01-07 17:30:22 -0500

    remove unused code, create ul button

--- js/scripts.js
+++ js/scripts.js
@@ -37,38 +37,16 @@ let pokemonRepository = (function (pokemon) {
       
 })()
 
-/*print pokemon names + heights
-
-for (let i=0; i < pokemonList.length; i++){
-    document.write(pokemonList[i].name + pokemonList[i].height + "<br>")
-}
-
-+/
-
-// for loop for length of array */
-// set condition for specific key-value of objects in array */
-// <br> tag to display each object on a new line */
-
-    //for (let i=0; i < pokemonList.length; i++){
-        //if(pokemonList[i].height <= 10){
-            //document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br>");
-        //} else {
-            //document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Woah, that's HUGE!" + "<br>")
-        //}
-    //}
-
-
-//function to print array items and details
-//function printArrayDetails(){
-    //for (let i = 0; i < pokemonList.length; i++){
-        //document.write("<p>" + pokemonList[i].name + "</p>" + "<p> Height: " + pokemonList[i].height + "</p>" + "<p> Type: " + pokemonList[i].type + "</p>" + "<br>");
-    //}
-//}
-
 pokemonRepository.getAll().forEach(function (pokemon){
     if(pokemon.height <= 10){
         document.write("<p> Name: " + pokemon.name + "</p> Height: " + pokemon.height + "</p>" + "<p> Type: " + pokemon.type + "</p>" + "<br>");
     } else {
         document.write("<p> Name: " + pokemon.name + "</p>" + "<p> Height: " + pokemon.height +  "- Woah, that's HUGE! </p>" + "<p> Type: " + pokemon.type + "</p>" +"<br>")
     }
+
+    let container = document.querySelector(".pokemon-list");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    container.appendChild(button);
+
 });
