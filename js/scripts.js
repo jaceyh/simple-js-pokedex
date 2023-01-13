45b256d56b47e1a81bb350389fec6ee1564e951d
jaceyh <jacey.hallock@gmail.com>
2023-01-13 17:58:08 -0500

    add dynamics to modal

--- js/scripts.js
+++ js/scripts.js
@@ -95,6 +95,7 @@ function showModal(title, text) {
     let closeButtonElement = document.createElement('button');
     closeButtonElement.classList.add('modal-close');
     closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click',hideModal);
   
     let titleElement = document.createElement('h1');
     titleElement.innerText = title;
@@ -108,7 +109,26 @@ function showModal(title, text) {
     modalContainer.appendChild(modal);
 
     modalContainer.classList.add('is-visible');
+
+    modalContainer.addEventListener('click', (e) => {
+        let target = e.target;
+        if (target === modalContainer) {
+          hideModal();
+        }
+    });
 }
+
+function hideModal() {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
+  }
+
+window.addEventListener('keydown', (e) => {
+    let modalContainer = document.querySelector('#modal-container');
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();  
+    }
+});
   
 document.querySelector('#show-modal').addEventListener('click', () => {
     showModal('Modal title','Modal Content');
