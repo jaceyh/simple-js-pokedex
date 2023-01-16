02e2a0467efa1a058d8164c8d4e2a7af4e823063
jaceyh <jacey.hallock@gmail.com>
2023-01-16 14:42:22 -0500

    add last event listener for close modal

--- js/scripts.js
+++ js/scripts.js
@@ -92,6 +92,13 @@ let pokemonRepository = (function () {
         modal.appendChild(titleElement);
         modal.appendChild(contentElement);
         modalContainer.appendChild(modal);
+
+        modalContainer.addEventListener('click', (e) => {
+            let target = e.target;
+            if (target === modalContainer) {
+              hideModal();
+            }
+        });
     }
 
     //this shows the modal on click
@@ -126,43 +133,6 @@ pokemonRepository.loadList().then(function() {
 });
 });
 
-/*
-function showModal(title, text) {
-    let modalContainer = document.querySelector('#modal-container');
-  
-    // Clear all existing modal content
-    modalContainer.innerHTML = '';
-  
-    let modal = document.createElement('div');
-    modal.classList.add('modal');
-  
-    // Add the new modal content
-    let closeButtonElement = document.createElement('button');
-    closeButtonElement.classList.add('modal-close');
-    closeButtonElement.innerText = 'Close';
-    closeButtonElement.addEventListener('click',hideModal);
-  
-    let titleElement = document.createElement('h1');
-    titleElement.innerText = title;
-  
-    let contentElement = document.createElement('p');
-    contentElement.innerText = text;
-  
-    modal.appendChild(closeButtonElement);
-    modal.appendChild(titleElement);
-    modal.appendChild(contentElement);
-    modalContainer.appendChild(modal);
-
-    modalContainer.classList.add('is-visible');
-
-    modalContainer.addEventListener('click', (e) => {
-        let target = e.target;
-        if (target === modalContainer) {
-          hideModal();
-        }
-    });
-}
-*/
 
 function hideModal() {
     let modalContainer = document.querySelector('#modal-container');
