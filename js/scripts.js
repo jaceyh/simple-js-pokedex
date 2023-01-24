d85a708fbd60026298002277475feb7cc8e53898
jaceyh <jacey.hallock@gmail.com>
2023-01-24 16:07:43 -0500

    implement list-group and add new showModal function
    modal not displaying

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,8 @@ let pokemonRepository = (function () {
 
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-    //other functions 
+
+    //Other Functions 
 
     //function to GET list from API
     function loadList() {
@@ -35,11 +36,11 @@ let pokemonRepository = (function () {
             item.height = details.height;
             item.types = details.types;
 
-
+    //catch errors function
         }).catch(function (e) {
             console.error(e);
         });
-      }
+    }
 
     //function to return all items in pokemonList
     function getAll (){
@@ -51,19 +52,64 @@ let pokemonRepository = (function () {
         pokemonList.push(pokemon);
     }
 
-    //function that adds pokemon to pokemonList in button format
+    /* ORIGINAL function that adds pokemon to pokemonList in button format
     function addListItem(pokemon){
         let container = document.querySelector('.pokemon-list');
         let button = document.createElement("button");
         button.innerText = pokemon.name;
-        button.classList.add("group-list-item");
+        button.classList.add("button-class");
         container.appendChild(button);
         button.addEventListener('click', function() {
             showDetails(pokemon);
-        }); 
+        });
+    }
+*/
+
+
+    function addListItem(pokemon){
+        let container = $('.pokemon-list')
+        let listItem = $('<li class="list-group-item"></li>');
+        let button = $('<button class="pokemon-button btn btn-info" data-target="#pokemon-modal" data-toggle="modal">' + pokemon.name + '</button>');
+
+        listItem.append(button);
+        container.append(listItem);
+
+    // event listener to show pokemon details when clicked 
+        button.on('click', function() {
+            showDetails(pokemon);
+        });   
     }
 
-    //function to create modal
+
+    // NEW showModal function for Bootstrap
+    function showModal(pokemon){
+        let modalBody = $('.modal-body');
+        let modalTitle = $('.modal-title');
+
+        //clear existing content of the modal
+        modalBody.empty();
+        modalTitle.empty();
+
+        //create element for name in modal content
+        let nameElement = $('<h1>' + pokemon.name + '</h1>');
+
+        //create element for image in modal content
+        let imgElement = $('<img class="modal-image">');
+        imgElement.attr("src", pokemon.imageUrl);
+
+        //create element for height in modal content
+        let heightElement = $('<p>' + 'Height: ' + pokemon.height + '</p>');       
+
+        //create element for height in modal content
+        let typeElement = $('<p>' + 'Type: ' + pokemon.types + '</p>');
+
+        modalTitle.append(nameElement);
+        modalBody.append(imgElement);
+        modalBody.append(heightElement);
+        modalBody.append(typeElement);
+    }
+
+    /* OLD function to create modal
     function showModal(title, text, image) {
         let modalContainer = document.querySelector('#modal-container');
         modalContainer.classList.add('is-visible');
@@ -101,24 +147,27 @@ let pokemonRepository = (function () {
 
         modalContainer.addEventListener('click', (e) => {
             let target = e.target;
-            if (target === modalContainer) {
-              hideModal();
-            }
-        });
-    }
+                if (target === modal-dialog) {
+                 hideModal();
+              }
+            });
+        }
+
 
     //this shows the modal on click
-    document.querySelector('#show-modal').addEventListener('click', () => {
+    document.querySelector('.pokemon-list').addEventListener('click', () => {
         showModal();
     });
 
 
+    */
+
     //function to show modal with pokemon name, details, img
     function showDetails(pokemon){
         loadDetails(pokemon).then(function() {
-            showModal(pokemon.name, "Height: " + pokemon.height, pokemon.imageUrl);
-            }
-        )};
+            showModal(pokemon);
+            });
+    }
 
 
     return {
@@ -128,7 +177,8 @@ let pokemonRepository = (function () {
         loadList: loadList,
         loadDetails: loadDetails,
         showDetails: showDetails,
-    }
+        showModal: showModal,
+    };
       
 })();
 
@@ -136,17 +186,21 @@ pokemonRepository.loadList().then(function() {
     //now the data is loaded!
     pokemonRepository.getAll().forEach(function (pokemon) {
         pokemonRepository.addListItem(pokemon);
-});
+    });
 });
 
+/* OLD - shouldn't need since Bootstrap has this functionality
 function hideModal() {
-    let modalContainer = document.querySelector('#modal-container');
+    let modalContainer = document.querySelector('#modal-dialog');
     modalContainer.classList.remove('is-visible');
   }
 
 window.addEventListener('keydown', (e) => {
-    let modalContainer = document.querySelector('#modal-container');
+    let modalContainer = document.querySelector('#modal-dialog');
     if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
       hideModal();  
     }
 });
+*/
+
+//$('#pokemonModal').modal(data-show);
